e33d1ada2036f76c8af5520bd1b56687a5104e6e
WAliensky <50817553+w164955@users.noreply.github.com>
2023-02-20 10:51:44 +0800

    Update ConnectivityTest.js

--- JavaScript/ConnectivityTest.js
+++ JavaScript/ConnectivityTest.js
@@ -1,15 +1,15 @@
 // 由yibeizipeini 提供，key 和小白脸大佬修改完善。
 
 let $ = {
-Bilibili :'https://www.bilibili.com',
-Baidu :'https://www.baidu.com',
-Youtube :'https://www.youtube.com/',
-Google :'https://www.google.com/generate_204',
-Github :'https://www.github.com'
+Baidu:'https://www.baidu.com',
+Google:'https://www.google.com/generate_204',
+Github:'https://www.github.com',
+Youtube:'https://www.youtube.com/',
+Bilibili:'https://www.bilibili.com'
 }
 
 !(async () => {
-await Promise.all([http('Baidu '),http('Bilibili '),http('Github '),http('Google '),http('Youtube ')]).then((x)=>{
+await Promise.all([http('Baidu'),http('Google'),http('Github'),http('Youtube'),http('Bilibili')]).then((x)=>{
 	$done({
     title: 'Network Connectivity Test',
     content: x.join('\n'),
@@ -22,9 +22,9 @@ await Promise.all([http('Baidu '),http('Bilibili '),http('Github '),http('Google
 function http(req) {
     return new Promise((r) => {
 			let time = Date.now();
-        $httpClient.post(req, (err, resp, data) => {
-            r(req.split(" ")[1]+
-						'\xa0\xa0\xa0\xa0\xa0\t: ' +
+        $httpClient.post($[req], (err, resp, data) => {
+            r(req +
+						'\xa0\xa0\xa0\t: ' +
 						(Date.now() - time)+' ms');
         });
     });
